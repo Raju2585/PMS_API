375725811e606397cf372319c7cdb28839facaf9
SuchithraJalda <SuchithraJalda@DESKTOP-U22BKEQ>
2024-09-16 11:26:44 +0530

    addingpropinmedical

--- PMS.Domain/Entities/DTOs/MedicalHistoryDTOs.cs
+++ PMS.Domain/Entities/DTOs/MedicalHistoryDTOs.cs
@@ -9,11 +9,27 @@ namespace PMS.Domain.Entities.DTOs
 {
     public class MedicalHistoryDTOs
     {
-        
+
         public int HistoryId { get; set; }
         public int PatientId { get; set; }
         public int DoctorId { get; set; }
         public DateTime RecordedDate { get; set; }
         public string Reason { get; set; }
+
+        public string DoctorName { get; set; }
+        public string HospitalName { get; set; }
+        public List<string> Medication { get; set; } = new List<string>();
+
+        // Boolean fields for Personal Medical History
+        public bool HasAsthma { get; set; }
+        public bool HasBloodPressure { get; set; }
+        public bool HasCancer { get; set; }
+        public bool HasCholesterol { get; set; }
+        public bool HasDiabetes { get; set; }
+        public bool HasHeartDisease { get; set; }
+
+
+
+
     }
 }
