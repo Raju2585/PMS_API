76d0f60887f9cb164d7f6f07f21dcef6c3aa80fd
SuchithraJalda <SuchithraJalda@DESKTOP-U22BKEQ>
2024-09-18 16:51:06 +0530

    changes

--- PMS.Domain/Entities/DTOs/MedicalHistoryDTOs.cs
+++ PMS.Domain/Entities/DTOs/MedicalHistoryDTOs.cs
@@ -16,8 +16,8 @@ namespace PMS.Domain.Entities.DTOs
         public DateTime RecordedDate { get; set; }
         public string Reason { get; set; }
 
-        public string DoctorName { get; set; }
-        public string HospitalName { get; set; }
+        //public string DoctorName { get; set; }
+        //public string HospitalName { get; set; }
         public List<string> Medication { get; set; } = new List<string>();
 
         // Boolean fields for Personal Medical History
@@ -27,7 +27,7 @@ namespace PMS.Domain.Entities.DTOs
         public bool HasCholesterol { get; set; }
         public bool HasDiabetes { get; set; }
         public bool HasHeartDisease { get; set; }
-        public string ExerciseFrequency { get; set; }
+        public string ExerciseFrequency { get; set; } = "Default Frequency";
 
         public string AlcoholConsumption { get; set; }
         public string Smoke { get; set; }
